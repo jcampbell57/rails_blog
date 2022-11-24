35be41a3a7b473e138627d61a31f78b3b74c9a7a
jcampbell <jcampbell57@gmail.com>
2022-11-23 21:37:44 -0800

    add commenting ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do
+    resources :comments
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
